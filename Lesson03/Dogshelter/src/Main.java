1c35395580cdf6b63b9540b0a2fc0604b74a3459
Tom Gelhorn <tom.gelhorn@gmail.com>
2024-05-13 10:46:42 +0200

    done group task 2

--- Lesson03/Dogshelter/src/Main.java
+++ Lesson03/Dogshelter/src/Main.java
@@ -1,4 +1,14 @@
+import static java.lang.Math.random;
+
 public class Main {
+    public static void strTest(Dog dog){
+        if (dog != null) {
+            System.out.println("I found " + dog.getName());
+        }
+        else{
+            System.out.println("No Dog found");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Running dog shelter main - part 1");
         String[] names = {
@@ -6,9 +16,34 @@ public class Main {
         };
         Shelter shelter = new Shelter(names.length);
         for (int i = 0; i < names.length; i++) {
-            Dog dog = new Dog(names[i], 70);
+            Dog dog = new Dog(names[i], (int)(random()*100));
             shelter.addDog(dog);
         }
+
+        //Search Test
+
+        Dog doggo = shelter.getDog("Timmy");
+        Main.strTest(doggo);
+        doggo = shelter.getDog("Tommy");
+        Main.strTest(doggo);
+
+        //Size Test
+
+        doggo = shelter.getDog(69);
+        if (doggo != null) {
+            System.out.println("I found " + doggo.getName() + ", with size: "+ doggo.getSize());
+        }
+        else{
+            System.out.println("No Dog found");
+        }
+
+        // Substring test
+
+
+        doggo = shelter.getDog("mm");
+        Main.strTest(doggo);
+        doggo = shelter.getDog("usi");
+        Main.strTest(doggo);
         shelter.print();
     }
 }
--- Lesson03/Dogshelter/src/Shelter.java
+++ Lesson03/Dogshelter/src/Shelter.java
@@ -3,28 +3,52 @@ public class Shelter {
     private int capacity;
     private Dog[] dogs;
 
-    public Shelter(int capacity){
+    public Shelter(int capacity) {
         this.capacity = capacity;
         count = 0;
-         dogs = new Dog[this.capacity];
+        dogs = new Dog[this.capacity];
     }
 
-
     public void addDog(Dog dog) {
-        if(count < capacity){
+        if (count < capacity) {
             dogs[count] = dog;
             count++;
-        }
-        else{
+        } else {
             return;
         }
     }
 
-    public void print(){
-        System.out.print("Capacity: "+ capacity + ", Count: "+ count + ", Dogs: ");
+    public void print() {
+        System.out.print("Capacity: " + capacity + ", Count: " + count + ", Dogs: ");
         for (int i = 0; i < count; i++) {
-            System.out.print(dogs[i].getName()+"(Size: "+dogs[i].getSize()+"), ");
+            System.out.print(dogs[i].getName() + "(Size: " + dogs[i].getSize() + "), ");
         }
         System.out.println();
+    }
+/*
+    public Dog getDog(String name) {
+        for (int i = 0; i < count; i++) {
+            if (dogs[i].getName().equals(name)) {
+                return dogs[i];
+            }
+        }
+        return null;
+    }
+*/
+    public Dog getDog(String name) {
+        for (int i = 0; i < count; i++) {
+            if (dogs[i].getName().contains(name)) {
+                return dogs[i];
+            }
+        }
+        return null;
+    }
+    public Dog getDog(int minSize) {
+        for (int i = 0; i < count; i++) {
+            if (dogs[i].getSize() >= minSize) {
+                return dogs[i];
+            }
+        }
+    return null;
     }
 }
